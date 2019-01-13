7add97624a0d554e0e210dcc76fb360b116ee481
Chris Dent <cdent@anticdent.org>
2019-01-13 18:31:15 +0000

    Test script for using libvirt via python
    Will incorporate this into the tools later.

---
+++ test.py
@@ -0,0 +1,19 @@
+
+import sys
+import libvirt
+
+conn = libvirt.open('qemu:///system')
+
+dom = conn.lookupByName(sys.argv[1])
+
+print(dom)
+
+ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE, 0)
+
+print(ifaces)
+
+
+for name, value in ifaces.items():
+    for ipaddr in value['addrs']:
+        print(ipaddr['addr'])
+
