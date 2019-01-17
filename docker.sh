ca4de6ec129fd540d67d0f11d5cef6951ef64c75
Chris Dent <cdent@anticdent.org>
2019-01-17 16:00:24 +0000

    Expose placement on wildcard interace and use remote image

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@ docker run -d -v /data/etcd/:/data \
           -p 2379:2379 -p 2380:2380 xieyanze/etcd3:latest
 # Placement is configued by the variables in dockerenv, including
 # pointing to an external database WHICH YOU MUST SET UP.
-docker run -dt -p 127.0.0.1:8080:80 --env-file dockerenv \
+docker run -dt -p 8080:80 --env-file dockerenv \
        cdent/placedock:latest
